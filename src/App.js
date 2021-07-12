ba066f54531946fbda9260b04192755a49e150a6
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-07-13 00:34:55 +0430

    solved

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@
 //import HookCounterThree from "./components/HookCounterThree";
 //import HookCounterTwo from "./components/HookCounterTwo";
 //import HookCounterOne from "./components/HookCounterOne";
+import DataFetching from "./components/DataFetching";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/*<HookCounter />
+      <HookCounterTwo />
+      <HookCounterThree />
+      <HookCounterFour />
+      <HookCounterOne />*/}
+      <DataFetching />
+    </div>
+  );
+}
+
+
+import React from "react";
+import UseReduceCounterTwo from "./components/UseReduceCounterTwo";
+//import UseReduceCounter from "./components/UseReduceCounter";
+//import ComponentC from "./components/ComponentC";
+//import UseReduceCounter from "./components/UseReduceCounter";
+import "./styles.css";
+//export const userContext = React.createContext();
+//export const ChannelContext = React.createContext();
+export default function App() {
+
 import React from "react";
 import UseReduceCounterTwo from "./components/UseReduceCounterTwo";
 //import UseReduceCounter from "./components/UseReduceCounter";
@@ -19,6 +46,26 @@ export default function App() {
       <HookCounterThree />
       <HookCounterFour />
       <HookCounterOne />
+      <userContext.Provider value={"mahsan"}>
+        <ChannelContext.Provider value={'codeEvolution'}>
+        <ComponentC />
+        </ChannelContext.Provider>
+      </userContext.Provider>
+      <UseReduceCounter />*/}
+      <UseReduceCounterTwo />
+    </div>
+  );
+}
+
+
+  return (
+    <div className="App">
+      {/*<HookCounter />
+      <HookCounterTwo />
+      <HookCounterThree />
+      <HookCounterFour />
+      <HookCounterOne />
+>>>>>>> GitHub
       <userContext.Provider value={"mahsan"}>
         <ChannelContext.Provider value={'codeEvolution'}>
         <ComponentC />
---
+++ src/components/DataFetching.js
@@ -0,0 +1,6 @@
+//useReducer for data fetching
+import React,{useEffect,useReducer} from 'react';
+function DataFetching(){
+
+}
+export default DataFetching;
