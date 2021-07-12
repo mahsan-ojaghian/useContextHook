d44fe7c4d7d1c8b7ef75e1187f26e1c5935e1569
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-07-13 02:19:26 +0430

    completed.

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -13,9 +13,43 @@ const initialState = {
 
 //reducer function
 const reducer = (state,action)=>{
- 
+  
+ switch(action.type){
+// .then()
+case 'FETCH_SUCCESSFUL' :
+  return {
+    loading : false,
+    posts : action.payload,
+    error : ''
+  }
+  // .catch()
+case 'FETCH_ERROR' :
+return {
+  loading : false,
+  posts : {},
+  error : 'something went wrong'
+}
+default :
+return state
+ }
 }
 function DataFetching(){
+const [state,dispatch] = useReducer(reducer,initialState);
 
+//useEffect()
+useEffect(()=>{
+axios.get('https://jsonplaceholder.typicode.com/posts')
+.then(response => dispatch({type : 'FETCH_SUCCESSFUL',payload : response.data}))
+.catch(error => dispatch({type : 'FETCH_ERROR'}))
+},[])
+
+
+//JSX
+return (
+  <div>
+{state.loading ?  'loading' : state.posts.title}
+{state.error ? state.error : null}
+  </div>
+)
 }
 export default DataFetching;
